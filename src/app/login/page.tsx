42ca907b14e07f33ebdd490a234ce0fcc20dcc9a
devlyst <dimakuzmintcev@gmail.com>
2025-12-26 20:56:16 +0300

    start project

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="flex min-h-screen justify-center items-center">
+      <div className="w-2xl h-2xl border-2">wdawdawdaw</div>
+    </div>
+  );
+}
