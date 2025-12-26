c16e75cce6bf55db7f014132a455816db626af81
devlyst <dimakuzmintcev@gmail.com>
2025-12-26 21:22:47 +0300

    add login window

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,27 @@ import React from "react";
 export default function page() {
   return (
     <div className="flex min-h-screen justify-center items-center">
-      <div className="w-2xl h-2xl border-2">wdawdawdaw</div>
+      <div className="w-100 h-100 border-2 p-10">
+        <div className="flex gap-5">
+          <button className="text-2xl mb-5 border-2 hover:bg-amber-100">
+            Авторизация
+          </button>
+          <button className="text-2xl mb-5 border-2 hover:bg-amber-100">
+            Регистрация
+          </button>
+        </div>
+        <div className="flex flex-col gap-10 mb-10">
+          <div className="flex flex-col">
+            <span>Логин</span>
+            <input className="border-1" />
+          </div>
+          <div className="flex flex-col">
+            <span>Пароль</span>
+            <input className="border-1" />
+          </div>
+        </div>
+        <button className="border-1 w-50 hover:bg-amber-100">Войти</button>
+      </div>
     </div>
   );
 }
